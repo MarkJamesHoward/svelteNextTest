7529002d1e96639e3eecb76857e91367e0060e6a
MarkJamesHoward <markjameshoward@hotmail.co.uk>
2020-10-21 13:23:33 +1300

    initial

---
+++ src/components/Timer.svelte
@@ -0,0 +1,29 @@
+<script>
+
+export let time = 0;
+
+const pause = () => {
+  clearInterval(timer);
+}
+
+let timer = setInterval(() => {
+  time++;
+}, 1000)
+
+</script>
+
+<div>
+  <h1>
+  {time}
+  </h1>
+
+  <button on:click={pause}>
+  Pause</button>
+</div>
+
+
+<style>
+h1 {
+  background: orange;
+}
+</style>
